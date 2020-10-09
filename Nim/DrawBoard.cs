2dac790fe1eb859cbd2578fd4f5afdc393b3b041
David <davidjordan86@gmail.com>
2020-10-09 14:42:59 -0500

    Updated visual studio project, created DrawBoard class

---
+++ Nim/DrawBoard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nim
+{
+    class DrawBoard
+    {
+        public void RenderBoard(Heap[] HeapArray)
+        {
+            int LargestPile = 0;
+            foreach (Heap heap in HeapArray)
+            {
+                if (heap.NumberOfObjects >= LargestPile)
+                {
+                    LargestPile = heap.NumberOfObjects;
+                }
+            }
+
+            for (int i = 0; i < LargestPile + 1; i++)
+            {
+                for (int j = 0; j < HeapArray.Length; j++)
+                {
+                    if (LargestPile - i >= HeapArray[j].NumberOfObjects)
+                    {
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        Console.Write(LargestPile - i + " ");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ Nim/Heap.cs
@@ -0,0 +1,33 @@
+﻿namespace Nim
+{
+    class Heap
+    {
+        // Keeps track of the number of objects left in this heap
+        private int _stones;
+
+        // Sets initial number of objects on creation
+        public Heap(int numberOfStones)
+        {
+            _stones = numberOfStones;
+        }
+
+        // Public property for getting the number of objects left
+        public int NumberOfObjects
+        {
+            get => _stones;
+        }
+
+        // Only valid move is to reduce stones, so this is the function other classes will use
+        // while playing the game.
+        // Returns the number of stones removed from the pile, if no stones removed, returns 0
+        public int RemoveStones(int number)
+        {
+            if (_stones > 0 && _stones - number >= 0)
+            {
+                _stones -= number;
+                return number;
+            }
+            return 0;
+        }
+    }
+}
